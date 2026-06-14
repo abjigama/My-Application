77fb481194dac7faadd36cc298070412607f4539
abjigama <abbjigama@gmail.com>
2026-06-14 06:38:36 -0600

    update

--- flutter_application_1/lib/delete_account.dart
+++ flutter_application_1/lib/delete_account.dart
@@ -4,10 +4,13 @@ import 'login.dart';
 
 class DeleteAccount extends StatelessWidget {
   const DeleteAccount({super.key});
-  
+
   
   @override
   Widget build(BuildContext context) {
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(title: Text("Delete Account"),
       centerTitle: true,),
@@ -16,19 +19,32 @@ class DeleteAccount extends StatelessWidget {
           children: [
            SizedBox(height: 20),
           Text("Email.Address"),
-          TextFormField(),
+          TextFormField(
+            controller: emailController,
+          ),
+
           SizedBox(height: 20),
           Text("Current Password"),
-          TextFormField(), 
+          TextFormField(
+            controller: passwordController,
+              obscureText: true,// Hides the password typing
+          ), 
           SizedBox(height: 20),
           Text("Are you sure you want to delete your account? This action is permanent and cannot be undone."),
           ElevatedButton(
-            onPressed: () {
-             Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(builder: (context) => Login()),
-                (route) => false,
-              );;
+            onPressed: () async {
+              try{
+                await authServices.value.deleteAccount 
+                (email:emailController.text,password:  passwordController.text,); 
+              
+               Navigator.pushAndRemoveUntil(
+                 context,
+                 MaterialPageRoute(builder: (context) => Login()),
+                 (route) => false,
+                );
+              } catch (e){
+                print("Error deleting account: $e");
+              } 
             },
             child: Text("Delete Permanently"),
           ),
